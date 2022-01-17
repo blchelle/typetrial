c2b9188ad076ec6faf555f2f8eea9125eb95037d
Brock Chelle <brocklchelle@gmail.com>
2022-01-17 16:16:23 -0700

    #5: Ping the API from the Client (#6)
    Also setup client side routing and path aliases

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
-import TypingZone from "./Components/TypingZone";
+import Router from "@components/Router";
+import axios from "@config/axios";
+import { useState } from "react";
+
 function App() {
+  const [randomServerNumber, setRandomServerNumber] = useState(null);
+
+  const sendRequest = async () => {
+    let res = await axios.get("/api/random");
+    setRandomServerNumber(res.data);
+  };
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <TypingZone />
+    <div className="w-screen h-screen flex justify-center items-center bg-gray-100 flex-col">
+      <Router />
+      <button className="bg-green-500 text-white p-4 rounded-lg mt-10" onClick={sendRequest}>
+        Click Me to Ping the API
+      </button>
+      {randomServerNumber && <div>The API responded with {randomServerNumber}</div>}
     </div>
   );
 }
