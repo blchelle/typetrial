e44ae2323c858dceaad22fca36bdbc2c8915993c
Brock Chelle <brocklchelle@gmail.com>
2022-01-20 23:11:55 -0700

    #15: Setup linting and actions for server linting & building.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,20 @@
-import path from "path";
-import { randomInt } from "crypto";
+import path from 'path';
+import { randomInt } from 'crypto';
 
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 import expressWs from 'express-ws';
-import bodyParser from "body-parser";
-import cors from "cors";
-import helmet from "helmet";
-import hpp from "hpp";
-import rateLimit from "express-rate-limit";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import rateLimit from 'express-rate-limit';
 
-import environment from "./config/environment";
+import environment from './config/environment';
 
 // Has to be done in a 'require' because there are no type declarations
-const xss = require("xss-clean");
+const xss = require('xss-clean');
 
-const app = expressWs(express()).app
+const { app } = expressWs(express());
 
 const initMiddleware = () => {
   app.use(bodyParser.json());
@@ -24,9 +24,9 @@ const initMiddleware = () => {
     const limit = rateLimit({
       max: rateLimitConfig.maxRequests,
       windowMs: rateLimitConfig.timeWindow,
-      message: "Too many requests, try again later",
+      message: 'Too many requests, try again later',
     });
-    app.use("/api", limit);
+    app.use('/api', limit);
   });
 
   environment.clientUrls.forEach((url) => {
@@ -40,32 +40,32 @@ const initMiddleware = () => {
 
 initMiddleware();
 
-app.get("/api/random", (req: Request, res: Response) => {
+app.get('/api/random', (req: Request, res: Response) => {
   console.log(req.query);
   res.json(randomInt(100).toString());
 });
 
-app.ws("/api/random", (ws, _) => {
+app.ws('/api/random', (ws) => {
   setInterval(() => {
     ws.send(randomInt(100).toString());
   }, 2000);
 
   ws.on('close', () => {
-      console.log('WebSocket was closed')
-  })
-})
+    console.log('WebSocket was closed');
+  });
+});
 
-app.post("/api/echo", (req: Request, res: Response) => {
+app.post('/api/echo', (req: Request, res: Response) => {
   console.log(req.body);
   res.json(req.body);
 });
 
-app.use(express.static(path.join(__dirname, "../..", "client", "dist")));
+app.use(express.static(path.join(__dirname, '../..', 'client', 'dist')));
 app.use((_: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../..", "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, '../..', 'client', 'dist', 'index.html'));
 });
 
 // Start server on port 8080
 app.listen(8080, () => {
-  console.log("Server started on port 8080");
+  console.log('Server started on port 8080');
 });
