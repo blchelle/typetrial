c8b35ffc33090db3df6096422b7f91cb46ef8479
drojasca <drojasca@ualberta.ca>
2022-01-22 15:18:46 -0700

    multi-client chat

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,18 +3,42 @@ import { randomInt } from 'crypto';
 
 import express, { Request, Response } from 'express';
 import expressWs from 'express-ws';
+import WebSocket from 'ws';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
 import hpp from 'hpp';
 import rateLimit from 'express-rate-limit';
 
+import { v4 as uuid } from 'uuid';
 import environment from './config/environment';
 
 // Has to be done in a 'require' because there are no type declarations
 const xss = require('xss-clean');
 
 const { app } = expressWs(express());
+
+interface UserInfo {
+  username: string;
+  room: string;
+}
+
+const rooms = new Map<string, string[]| undefined>();
+const users = new Map<string, WebSocket | undefined>();
+
+const sendMessage = (sender: string, message: string, dest: string[] | undefined) => {
+  dest?.forEach((user) => {
+    users.get(user.toString())?.send(JSON.stringify({ sender, data: message }));
+  });
+};
+
+const getRoom = () => {
+  if (rooms.size === 0) {
+    rooms.set(uuid(), <string[]>[]);
+  }
+
+  return rooms.keys().next().value;
+};
 
 const initMiddleware = () => {
   app.use(bodyParser.json());
@@ -45,13 +69,32 @@ app.get('/api/random', (req: Request, res: Response) => {
   res.json(randomInt(100).toString());
 });
 
-app.ws('/api/random', (ws) => {
-  setInterval(() => {
-    ws.send(randomInt(100).toString());
-  }, 2000);
+app.ws('/api/connect/:user', (ws, req: Request) => {
+  console.log('Connected');
+
+  const room = getRoom();
+  rooms.get(room)?.push(req.params.user);
+  users.set(req.params.user, ws);
+
+  const userInfo: UserInfo = { username: req.params.user, room };
+
+  ws.on('message', (msg) => {
+    const roomUsers = rooms.get(userInfo.room);
+    const dest = roomUsers?.filter((word) => word.toString() !== userInfo.username.toString());
+
+    sendMessage(userInfo.username, msg.toString(), dest);
+  });
 
   ws.on('close', () => {
     console.log('WebSocket was closed');
+    users.delete(userInfo.username);
+    const remaining = rooms.get(userInfo.room)?.filter((user) => user !== userInfo.username);
+
+    if (remaining?.length === 0) {
+      rooms.delete(userInfo.room);
+    } else {
+      rooms.set(userInfo.room, remaining);
+    }
   });
 });
 
