f95f1b9db7a795bd2b81701938ce4636417900e5
Brock Chelle <brocklchelle@gmail.com>
2022-02-01 17:15:21 -0700

    #28: Extend <Button /> component

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,46 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface ButtonProps {
   text: string;
-  onClick: () => void;
+  isAsync?: boolean;
+  isDisabled?: boolean;
+  onClick: (callback?: () => void) => void;
   color?: 'primary' | 'secondary';
-  size?: 'small' | 'medium' | 'large' | 'fit';
   type?: 'solid' | 'border' | 'ghost';
   style?: React.CSSProperties;
   Icon?: React.FC<React.SVGProps<SVGSVGElement>>;
 }
 
 const Button: React.FC<ButtonProps> = ({
-  text, onClick, color = 'primary', type = 'solid', size = 'medium', style, Icon,
+  color = 'primary',
+  Icon,
+  isAsync = false,
+  isDisabled = false,
+  onClick,
+  style,
+  text,
+  type = 'solid',
 }) => {
-  const getColorClass = () => {
-    // Ghost cancels out the color class
-    if (type === 'ghost') return 'bg-transparent hover:bg-gray-100 text-gray-700';
+  const [isLoading, setIsLoading] = useState(false);
 
-    if (color === 'primary' && type === 'solid') return 'bg-primary hover:bg-primary-dark';
-    if (color === 'primary' && type === 'border') return 'border-primary hover:bg-primary border-2 text-primary hover:text-white';
-    if (color === 'secondary' && type === 'solid') return 'bg-secondary hover:bg-secondary-dark';
-    if (color === 'secondary' && type === 'border') return 'border-secondary hover:bg-secondary border-2 text-secondary hover:text-white';
+  const dynamicButtonStyles = () => {
+    const classes = [];
+    if (isDisabled || isLoading) classes.push('cursor-auto');
 
-    return '';
+    if (type === 'border') classes.push('border-2');
+    if (type === 'solid') classes.push('text-white');
+
+    if (isDisabled && (type === 'border' || type === 'ghost')) classes.push('text-gray-400');
+    if (!isDisabled && (type === 'border' || type === 'ghost')) classes.push(`text-${color}`);
+    if (!isDisabled && !isLoading && (type === 'border' || type === 'ghost')) classes.push(`hover:bg-${color}-transparent`);
+
+    if (isDisabled && type === 'border') classes.push('border-gray-400');
+    if (!isDisabled && type === 'border') classes.push(`border-${color}`);
+
+    if (isDisabled && type === 'solid') classes.push('bg-gray-400');
+    if (!isDisabled && type === 'solid') classes.push(`bg-${color}`, `hover:bg-${color}`);
+
+    return classes.join(' ');
   };
 
-  const getSizeClass = () => {
-    if (size === 'small') return 'w-max h-8';
-    if (size === 'medium') return 'w-max h-12';
-    if (size === 'large') return 'w-max h-16';
+  const dynamicLoadingStyles = () => {
+    if (type === 'solid') return `loader-${color}`;
+    return `loader-${color}-transparent`;
+  };
 
-    return 'w-max h-auto';
+  const handleClick = async () => {
+    if (isDisabled || isLoading) return;
+
+    if (isAsync) {
+      setIsLoading(true);
+      onClick(() => setIsLoading(false));
+    } else {
+      onClick();
+    }
   };
 
   return (
     <button
-      className={`flex items-center justify-between text-md text-white px-6 py-2 font-bold rounded-lg cursor-pointer transition ${getColorClass()} ${getSizeClass()}`}
+      className={`relative flex items-center justify-between w-fit h-fit px-4 py-2 font-bold rounded-lg transition overflow-hidden ${dynamicButtonStyles()}`}
       style={style}
-      onClick={onClick}
+      onClick={handleClick}
+      disabled={isDisabled}
     >
-      {text}
-      {Icon && <Icon className="h-10 w-10 ml-4 text-gray-1 fill-current" />}
+      { isLoading && <div className={`absolute top-0 left-0 w-3/2 h-full animate-load z-10 ${dynamicLoadingStyles()}`} />}
+      <p className="z-10">{text}</p>
+      {Icon && <Icon className="z-10 h-fit w-6 ml-2 text-gray-1 fill-current" />}
     </button>
   );
 };
--- client/src/components/ButtonsShowcase.tsx
+++ client/src/components/ButtonsShowcase.tsx
@@ -1,33 +1,50 @@
 import React from 'react';
+import { IoCheckmarkCircle } from 'react-icons/io5';
+
 import Button from '@components/Button';
+
+const simulateLoad = (callback?: () => void) => setTimeout(callback!, 3000);
 
 const ButtonsShowcase: React.FC = () => (
   <>
-    <h1 className="text-3xl mb-4">Buttons Showcase</h1>
-    <div className="bg-gray-200 grid grid-cols-4 p-16 gap-4 border-4 border-gray-300 rounded-lg items-center">
+    <h1 className="text-3xl">Buttons Showcase</h1>
+    <h3 className="text-md text-gray-500">Click buttons to see loading animation</h3>
+    <div className="bg-gray-100 grid grid-cols-3 p-16 gap-4 border-4 border-gray-200 rounded-lg items-center">
       <p className="font-bold" />
       <p className="font-bold">Primary</p>
       <p className="font-bold">Secondary</p>
-      <p className="font-bold">Ghost</p>
-      <p className="font-bold">Small</p>
-      <Button text="Button Text" size="small" onClick={() => {}} />
-      <Button text="Button Text" size="small" color="secondary" onClick={() => {}} />
-      <Button text="Button Text" size="small" type="ghost" onClick={() => {}} />
-      <p className="font-bold">Medium</p>
-      <Button text="Button Text" size="medium" onClick={() => {}} />
-      <Button text="Button Text" size="medium" color="secondary" onClick={() => {}} />
-      <Button text="Button Text" size="medium" type="ghost" onClick={() => {}} />
-      <p className="font-bold">Large</p>
-      <Button text="Button Text" size="large" onClick={() => {}} />
-      <Button text="Button Text" size="large" color="secondary" onClick={() => {}} />
-      <Button text="Button Text" size="large" type="ghost" onClick={() => {}} />
-      <p className="font-bold">Fit</p>
-      <Button text="Button Text" size="fit" onClick={() => {}} />
-      <Button text="Button Text" size="fit" color="secondary" onClick={() => {}} />
-      <Button text="Button Text" size="fit" type="ghost" onClick={() => {}} />
-      <p className="font-bold">Outlined</p>
-      <Button text="Button Text" size="fit" type="border" onClick={() => {}} />
-      <Button text="Button Text" size="fit" color="secondary" type="border" onClick={() => {}} />
+
+      <p className="font-bold">Solid</p>
+      <Button text="Button Text" onClick={simulateLoad} isAsync />
+      <Button text="Button Text" color="secondary" onClick={simulateLoad} isAsync />
+
+      <p className="font-bold">Solid Disabled</p>
+      <Button text="Button Text" onClick={simulateLoad} isDisabled />
+      <Button text="Button Text" color="secondary" onClick={simulateLoad} isDisabled />
+
+      <p className="font-bold">Border</p>
+      <Button text="Button Text" type="border" onClick={simulateLoad} isAsync />
+      <Button text="Button Text" type="border" color="secondary" onClick={simulateLoad} isAsync />
+
+      <p className="font-bold">Border Disabled</p>
+      <Button text="Button Text" type="border" onClick={simulateLoad} isDisabled />
+      <Button text="Button Text" color="secondary" type="border" onClick={simulateLoad} isDisabled />
+
+      <p className="font-bold">Ghosts</p>
+      <Button text="Button Text" type="ghost" onClick={simulateLoad} isAsync />
+      <Button text="Button Text" color="secondary" type="ghost" onClick={simulateLoad} isAsync />
+
+      <p className="font-bold">Ghosts Disabled</p>
+      <Button text="Button Text" type="ghost" onClick={simulateLoad} isDisabled />
+      <Button text="Button Text" color="secondary" type="ghost" onClick={simulateLoad} isDisabled />
+
+      <p className="font-bold">With Icon</p>
+      <Button text="Button Text" onClick={simulateLoad} isAsync Icon={IoCheckmarkCircle} />
+      <Button text="Button Text" color="secondary" onClick={simulateLoad} isAsync Icon={IoCheckmarkCircle} />
+
+      <p className="font-bold">With Icon Disabled</p>
+      <Button text="Button Text" onClick={simulateLoad} isDisabled Icon={IoCheckmarkCircle} />
+      <Button text="Button Text" color="secondary" onClick={simulateLoad} isDisabled Icon={IoCheckmarkCircle} />
     </div>
   </>
 );
