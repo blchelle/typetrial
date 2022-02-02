c989b230a57aa8b64b3e410162648ef5f3d57162
Brock Chelle <brocklchelle@gmail.com>
2022-02-01 17:15:21 -0700

    #28: Extend <Button /> component

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: [
+    {
+      pattern: /(bg|text|border)-(primary|secondary)(-(light|dark|transparent))?/,
+      variants: ['hover'],
+    },
+  ],
   theme: {
     extend: {
       colors: {
@@ -7,10 +13,12 @@ module.exports = {
           light: '#14E1BF',
           DEFAULT: '#2CC9A7',
           dark: '#3EBDA1',
+          transparent: '#3EBDA122',
         },
         secondary: {
           DEFAULT: '#2191ED',
           dark: '#107CE8',
+          transparent: '#107CE822',
         },
         error: {
           DEFAULT: '#D06262',
@@ -18,13 +26,19 @@ module.exports = {
       },
       animation: {
         blink: 'blink 1s linear infinite',
+        load: 'load 1s infinite linear',
       },
       keyframes: {
         blink: {
           '0%, 49%, 100%': { opacity: 1 },
           '50%, 99%': { opacity: 0 },
         },
+        load: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-1.75rem)' },
+        },
       },
+      width: { '3/2': '150%' },
     },
   },
   plugins: [],
