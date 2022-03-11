7db421bcaed062768c5844e65c6cb21825217224
Daniel Rojas-Cardona <44874031+drojasca@users.noreply.github.com>
2022-03-10 20:48:08 -0700

    setup initial socket architecture (#57)
    * setup initial socket architecture
    
    * fixed linting issues

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,9 +5,7 @@ import rateLimit from 'express-rate-limit';
 import expressWs from 'express-ws';
 import path from 'path';
 import hpp from 'hpp';
-import { WebSocket } from 'ws';
-import { v4 as uuid } from 'uuid';
-
+import WsHandler from './websocketHandler';
 import environment from './config/environment';
 import apiRoutes from './routes/apiRoutes';
 import { openLogFiles, writeLog } from './utils/log';
@@ -24,22 +22,7 @@ interface UserInfo {
 
 const { app } = expressWs(express());
 
-const rooms = new Map<string, string[]| undefined>();
-const users = new Map<string, WebSocket | undefined>();
-
-const sendMessage = (sender: string, message: string, dest: string[] | undefined) => {
-  dest?.forEach((user) => {
-    users.get(user.toString())?.send(JSON.stringify({ sender, data: message }));
-  });
-};
-
-const getRoom = () => {
-  if (rooms.size === 0) {
-    rooms.set(uuid(), <string[]>[]);
-  }
-
-  return rooms.keys().next().value;
-};
+const wsHandler = new WsHandler(2);
 
 const initMiddleware = () => {
   app.use(bodyParser.json());
@@ -65,40 +48,26 @@ const initMiddleware = () => {
   app.use(xss());
 };
 
+app.ws('/api/connect/:user', (ws, req: Request) => {
+  writeLog({ event: 'user connected to websocket', user: req.params.user }, 'info');
+
+  const room = wsHandler.connect_user_to_room(req.params.user, ws);
+  const userInfo: UserInfo = { username: req.params.user, room };
+
+  ws.on('message', (msg) => {
+    wsHandler.broadcast_message(userInfo.room, userInfo.room, msg.toString(), true);
+  });
+
+  ws.on('close', () => {
+    writeLog({ event: 'websocket closed', user: userInfo.username }, 'info');
+    wsHandler.disconnect_user_from_room(userInfo.username, userInfo.room);
+  });
+});
+
 const main = async () => {
   initMiddleware();
   app.use('/api', apiRoutes);
   await openLogFiles();
-
-  app.ws('/api/connect/:user', (ws, req: Request) => {
-    writeLog({ event: 'user connected to websocket', user: req.params.user }, 'info');
-
-    const room = getRoom();
-    rooms.get(room)?.push(req.params.user);
-    users.set(req.params.user, ws);
-
-    const userInfo: UserInfo = { username: req.params.user, room };
-
-    ws.on('message', (msg) => {
-      const roomUsers = rooms.get(userInfo.room);
-      const dest = roomUsers?.filter((word) => word.toString() !== userInfo.username.toString());
-
-      writeLog({ event: 'user sent message', user: userInfo.username, message: msg }, 'info');
-      sendMessage(userInfo.username, msg.toString(), dest);
-    });
-
-    ws.on('close', () => {
-      writeLog({ event: 'websocket closed', user: userInfo.username }, 'info');
-      users.delete(userInfo.username);
-      const remaining = rooms.get(userInfo.room)?.filter((user) => user !== userInfo.username);
-
-      if (remaining?.length === 0) {
-        rooms.delete(userInfo.room);
-      } else {
-        rooms.set(userInfo.room, remaining);
-      }
-    });
-  });
 
   app.use(express.static(path.join(__dirname, '../..', 'client', 'dist')));
   app.use((_: Request, res: Response) => {
