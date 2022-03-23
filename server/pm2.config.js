31810b0ed04333647ef4e89e3301aa328f843990
Brock Chelle <brocklchelle@gmail.com>
2022-03-23 17:44:17 -0600

    #95: Fix production deploy

--- server/pm2.config.js
+++ server/pm2.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  apps : [{
-    name   : "server",
-    script : "./dist/server.js"
-  }]
-}
+  apps: [{
+    name: 'server',
+    script: './dist/src/server.js',
+  }],
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -47,9 +47,9 @@ const main = async () => {
   app.use('/api', apiRoutes);
   await openLogFiles();
 
-  app.use(express.static(path.join(__dirname, '../..', 'client', 'dist')));
+  app.use(express.static(path.join(__dirname, '../../..', 'client', 'dist')));
   app.use((_: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '../..', 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../..', 'client', 'dist', 'index.html'));
   });
 
   app.use(errorMiddleware);
