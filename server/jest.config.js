aba10035b116f4caef5e133390d5ee13dfc2de56
Brock Chelle <brocklchelle@gmail.com>
2022-03-31 11:48:04 -0600

    #132: Control race countdown from server

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   clearMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./src/spec/prismaMock.ts'],
+  setupFilesAfterEnv: ['./src/spec/prismaMock.ts', './src/spec/dateMock.ts'],
   testRegex: 'src/spec/.*\\.spec\\.ts$',
 };
