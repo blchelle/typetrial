549fe3e72db7c2e9672a82f1e5375413fa46149f
Samuel Lojpur <samlojpur@gmail.com>
2022-04-08 19:47:54 -0600

    readme update + fixes to client tests (#175)
    * readme update + fixes to client tests
    
    * add newline lol
    
    * add repo link

--- client/jest.config.js
+++ client/jest.config.js
@@ -8,9 +8,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
   setupFilesAfterEnv: ['./src/spec/setupSpecs.ts', './src/spec/axiosMock.ts', 'jest-localstorage-mock'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: path.join(__dirname, 'src') }),
+  moduleNameMapper: { ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: path.join(__dirname, 'src') }), '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/spec/fileMock.js' },
   testRegex: 'src/spec/.*\\.spec\\.tsx?$',
   transform: {
-    ".+\\.(css|scss|png|jpg|svg)$": "jest-transform-stub",
+    '.+\\.(css|scss|png|jpg|svg)$': 'jest-transform-stub',
   },
 };
